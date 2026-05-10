10e2db3fa5fdd0c25f0bda0e40c4d529a22902ff
Naveen <naveen@Mac.lan>
2026-05-10 20:31:57 +0530

    Fix: Add missing Link import in Home.tsx
    - Added Link import from react-router-dom
    - Fixes 'Link is not defined' error
    - View All Products button now properly navigates to /products page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import PromotionBanner from '../components/PromotionBanner';
 import CategoryGrid from '../components/CategoryGrid';
